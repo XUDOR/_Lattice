d1c6cfa2a519461498133d99aa8c19982c15c541
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-14 14:09:33 -0700

    modularization for PostQueries

--- db/queries/postsQueries.js
+++ db/queries/postsQueries.js
@@ -1,18 +1,9 @@
 const db = require('../connection');
+const createPost = require('./createPost'); // Adjust the path as needed
+const getAllPosts = require('./getAllPosts');
+const getPostsBySearch = require('./getPostsBySearch');
+const getPostById = require('./getPostById');
 
-const getAllPosts = async () => {
-  try {
-    const postsResult = await db.query(`
-      SELECT posts.*, users.username 
-      FROM posts 
-      JOIN users ON posts.user_id = users.id 
-      ORDER BY posts.created_at DESC
-    `);
-    return postsResult.rows;
-  } catch (err) {
-    console.error('Error in getAllPosts:', err.stack);
-    throw err;
-  }
-};
 
-module.exports = { getAllPosts };
+module.exports = { getAllPosts, createPost, getPostsBySearch, getPostById };
+
